ea8c9294ef3d8d52a8d21a2eb1c0698d51339f54
Steven Keith <ext_steven.keith@coats.com>
2022-10-13 19:47:07 +0100

    wrap tx hash in the table

--- src/pages/transaction.tsx
+++ src/pages/transaction.tsx
@@ -166,128 +166,142 @@ export default function Transaction({ address, user, txs }) {
                                 <Box>Tx Nonce: {user.nonce}</Box> 
                                 <Box>Tx Block Number: {user.blockNumber}</Box>                                
                             </Box> */}
-                            <TableContainer>
-                                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                                    <TableHead>
-                                        <TableRow>
-                                            <TableCell>Tx Hash</TableCell>
-                                            <TableCell>Time Processed</TableCell>
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        {txs.map(([hash, timestamp]) => (
-                                            <TableRow key={hash}>
-                                                <TableCell>
-                                                    <Link href={`https://mumbai.polygonscan.com/tx/${hash}`}>
-                                                        {hash}
-                                                    </Link>
-                                                </TableCell>
-                                                <TableCell>{format(fromUnixTime(timestamp / 1000), 'LLL do, yyyy, HH:mm:ss')}</TableCell>
-
+                            <Box sx={{ overflow: "auto" }}>
+                                <Box sx={{ width: "100%", display: "table", tableLayout: "fixed" }}>
+                                    <Table aria-label="simple table">
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell>Tx Hash</TableCell>
+                                                <TableCell>Time Processed</TableCell>
                                             </TableRow>
-                                        ))}
+                                        </TableHead>
+                                        <TableBody>
+                                            {txs.map(([hash, timestamp]) => (
+                                                <TableRow key={hash}>
+                                                    <TableCell>
+                                                        <Link href={`https://mumbai.polygonscan.com/tx/${hash}`}>
+                                                            <Typography 
+                                                            component="a" 
+                                                            sx={{
+                                                                display: 'inline-block',
+                                                                inlineSize: {
+                                                                    xs: 200,
+                                                                    sm: 300,
+                                                                    md: 400,
+                                                                    lg: 500,
+                                                                    xl: 700
+                                                                },
+                                                                overflowWrap: 'break-word'
+                                                            }}>{hash}</Typography>
+                                                        </Link>
+                                                    </TableCell>
+                                                    <TableCell>{format(fromUnixTime(timestamp / 1000), 'LLL do, yyyy, HH:mm:ss')}</TableCell>
+
+                                                </TableRow>
+                                            ))}
                                     </TableBody>
                                 </Table>
-                            </TableContainer>
+                            </Box>
                         </Box>
-                    </div>
-                    <div hidden={value !== 1}>
-                        <Box marginY={4}>
-                            <Alert
-                                severity="error"
-                                sx={{
-                                    display: 'flex',
-                                    justifyContent: 'center',
-                                    alignItems: 'center',
-                                    textAlign: 'center',
-                                    fontWeight: 600,
-                                    fontSize: {
-                                        xs: 14,
-                                        sm: 18
-                                    },
-                                    padding: 1,
-                                }}
-                            >
-                                Please only use this if support asks you to
-                            </Alert>
-                            <Typography marginY={2} sx={{ fontSize: 20, fontWeight: 600 }}>
-                                Queue a lost transaction
-                            </Typography>
-                            {/* <Box sx={{ paddingY: 1 }}>
+                    </Box>
+                </div>
+                <div hidden={value !== 1}>
+                    <Box marginY={4}>
+                        <Alert
+                            severity="error"
+                            sx={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                textAlign: 'center',
+                                fontWeight: 600,
+                                fontSize: {
+                                    xs: 14,
+                                    sm: 18
+                                },
+                                padding: 1,
+                            }}
+                        >
+                            Please only use this if support asks you to
+                        </Alert>
+                        <Typography marginY={2} sx={{ fontSize: 20, fontWeight: 600 }}>
+                            Queue a lost transaction
+                        </Typography>
+                        {/* <Box sx={{ paddingY: 1 }}>
                                 <Typography sx={{ fontWeight: 600, fontSize: 20, color: 'red' }}>READ CAREFULLY</Typography>
                             </Box> */}
 
-                            <Box sx={{ paddingY: 2 }}>
-                                <Typography mb={2} color={theme => theme.palette.text.secondary} sx={{ fontSize: 16 }}>
-                                    We only update our website once a transaction is confirmed on the blockchain.
-                                </Typography>
-                                <Typography mb={2} color={theme => theme.palette.text.secondary} sx={{ fontSize: 16 }}>
-                                    This form will NOT speed up the addition of your token to Hodl My Moon.
-                                </Typography>
+                        <Box sx={{ paddingY: 2 }}>
+                            <Typography mb={2} color={theme => theme.palette.text.secondary} sx={{ fontSize: 16 }}>
+                                We only update our website once a transaction is confirmed on the blockchain.
+                            </Typography>
+                            <Typography mb={2} color={theme => theme.palette.text.secondary} sx={{ fontSize: 16 }}>
+                                This form will NOT speed up the addition of your token to Hodl My Moon.
+                            </Typography>
 
+                        </Box>
+                        <hr />
+                        <Box sx={{ paddingY: 2 }}>
+                            <Typography mb={2} color={theme => theme.palette.text.secondary} sx={{ fontSize: 16 }}>
+                                If we&apos;ve missed one of your transactions, you can use this form to queue it for processing.
+                            </Typography>
+                        </Box>
+                        <hr />
+                        <Box sx={{ paddingY: 2 }}>
+                            <Typography mb={2} color={theme => theme.palette.text.secondary} sx={{ fontSize: 16 }}>
+                                Before submitting this form, ensure that:
+                            </Typography>
+                            <Box>
+                                <Typography component="li" mb={2} color={theme => theme.palette.text.secondary} sx={{ fontSize: 16 }}>We&apos;ve not already processed that transaction. See the tab at the top of this webpage</Typography>
+                                <Typography component="li" mb={2} color={theme => theme.palette.text.secondary} sx={{ fontSize: 16 }}>The transaction is for one of our contracts. (i.e. you initiated the transaction via Hodl My Moon)</Typography>
+                                <Typography component="li" mb={2} color={theme => theme.palette.text.secondary} sx={{ fontSize: 16 }}>The transaction isn&apos;t pending (i.e. it has been confirmed on the blockchain)</Typography>
+                                <Typography component="li" mb={2} color={theme => theme.palette.text.secondary} sx={{ fontSize: 16 }}>The transaction you are about to submit is the first one we&apos;ve missed</Typography>
                             </Box>
-                            <hr />
-                            <Box sx={{ paddingY: 2 }}>
-                                <Typography mb={2} color={theme => theme.palette.text.secondary} sx={{ fontSize: 16 }}>
-                                    If we&apos;ve missed one of your transactions, you can use this form to queue it for processing.
+                        </Box>
+                        <hr />
+                        <Box sx={{ paddingY: 2 }}>
+                            <Typography mb={2} color={theme => theme.palette.text.secondary} sx={{ fontSize: 16 }}>
+                                You can get your transaction ID in Metamask.
+                            </Typography>
+                            <Link target={"_blank"} href="https://metamask.zendesk.com/hc/en-us/articles/4413442094235-How-to-find-a-transaction-ID">
+                                <Typography color={theme => theme.palette.text.secondary} sx={{ fontSize: 16 }}>
+                                    How do I find my Transaction ID ?
                                 </Typography>
-                            </Box>
-                            <hr />
-                            <Box sx={{ paddingY: 2 }}>
-                                <Typography mb={2} color={theme => theme.palette.text.secondary} sx={{ fontSize: 16 }}>
-                                    Before submitting this form, ensure that:
-                                </Typography>
+                            </Link>
+                        </Box>
+                        <Box
+                            component="form"
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                textAlign: 'center'
+                            }}
+                        >
+                            <Box display="flex" alignItems="center" gap={2} sx={{ marginY: 4 }}>
                                 <Box>
-                                    <Typography component="li" mb={2} color={theme => theme.palette.text.secondary} sx={{ fontSize: 16 }}>We&apos;ve not already processed that transaction. See the tab at the top of this webpage</Typography>
-                                    <Typography component="li" mb={2} color={theme => theme.palette.text.secondary} sx={{ fontSize: 16 }}>The transaction is for one of our contracts. (i.e. you initiated the transaction via Hodl My Moon)</Typography>
-                                    <Typography component="li" mb={2} color={theme => theme.palette.text.secondary} sx={{ fontSize: 16 }}>The transaction isn&apos;t pending (i.e. it has been confirmed on the blockchain)</Typography>
-                                    <Typography component="li" mb={2} color={theme => theme.palette.text.secondary} sx={{ fontSize: 16 }}>The transaction you are about to submit is the first one we&apos;ve missed</Typography>
+                                    <TextField
+                                        sx={{ width: 250 }}
+                                        id="tx"
+                                        value={hash}
+                                        onChange={e => setHash(e.target.value)}
+                                        label="Transaction ID (Hash)"
+                                    />
                                 </Box>
-                            </Box>
-                            <hr />
-                            <Box sx={{ paddingY: 2 }}>
-                                <Typography mb={2} color={theme => theme.palette.text.secondary} sx={{ fontSize: 16 }}>
-                                    You can get your transaction ID in Metamask.
-                                </Typography>
-                                <Link target={"_blank"} href="https://metamask.zendesk.com/hc/en-us/articles/4413442094235-How-to-find-a-transaction-ID">
-                                    <Typography color={theme => theme.palette.text.secondary} sx={{ fontSize: 16 }}>
-                                        How do I find my Transaction ID ?
-                                    </Typography>
-                                </Link>
-                            </Box>
-                            <Box
-                                component="form"
-                                sx={{
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    textAlign: 'center'
-                                }}
-                            >
-                                <Box display="flex" alignItems="center" gap={2} sx={{ marginY: 4 }}>
-                                    <Box>
-                                        <TextField
-                                            sx={{ width: 250 }}
-                                            id="tx"
-                                            value={hash}
-                                            onChange={e => setHash(e.target.value)}
-                                            label="Transaction ID (Hash)"
-                                        />
-                                    </Box>
-                                    <Box>
-                                        <Button
-                                            sx={{ paddingY: 1.5, paddingX: 2.5, fontWeight: 600 }}
-                                            variant="contained"
-                                            onClick={() => sendTransaction()}>
-                                            Submit
-                                        </Button>
-                                    </Box>
+                                <Box>
+                                    <Button
+                                        sx={{ paddingY: 1.5, paddingX: 2.5, fontWeight: 600 }}
+                                        variant="contained"
+                                        onClick={() => sendTransaction()}>
+                                        Submit
+                                    </Button>
                                 </Box>
                             </Box>
                         </Box>
-                    </div>
-                </HodlBorderedBox>
-            </Box>
+                    </Box>
+                </div>
+            </HodlBorderedBox>
+        </Box>
         </>)
 }
