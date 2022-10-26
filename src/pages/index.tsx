9359ee90087ec22f1e2f6e6e5546049039cca561
Steven Keith <ext_steven.keith@coats.com>
2022-10-26 13:26:07 +0100

    this ALSO breaks the deployment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,133 +1,139 @@
-import Head from 'next/head';
-import dynamic from 'next/dynamic';
+// import Head from 'next/head';
+// import dynamic from 'next/dynamic';
 
-import Container from '@mui/material/Container';
-
-
-import { authenticate } from '../lib/jwt';
-import { FeedContext } from '../contexts/FeedContext';
-import { useActions } from '../hooks/useActions';
-import { ActionSet } from '../models/HodlAction';
-import { getActions } from './api/actions';
-import { useRankings } from '../hooks/useRankings';
-import { RankingsContext } from '../contexts/RankingsContext';
-import { getUser } from './api/user/[handle]';
-import { useNewUsers } from '../hooks/useNewUsers';
-import { UserContext } from '../contexts/UserContext';
-import { useFollowersCount } from '../hooks/useFollowersCount';
-import { useFollowingCount } from '../hooks/useFollowingCount';
-import { useHodlingCount } from '../hooks/useHodlingCount';
-import { useListedCount } from '../hooks/useListedCount';
-import { useNewTokens } from '../hooks/useNewTokens';
+// import Container from '@mui/material/Container';
 
 
-const PublicHomePage = dynamic(
-  () => import('../components/layout/PublicHomePage'),
-  {
-    loading: () => <div>...</div>
-  }
-);
+// import { authenticate } from '../lib/jwt';
+// import { FeedContext } from '../contexts/FeedContext';
+// import { useActions } from '../hooks/useActions';
+// import { ActionSet } from '../models/HodlAction';
+// import { getActions } from './api/actions';
+// import { useRankings } from '../hooks/useRankings';
+// import { RankingsContext } from '../contexts/RankingsContext';
+// import { getUser } from './api/user/[handle]';
+// import { useNewUsers } from '../hooks/useNewUsers';
+// import { UserContext } from '../contexts/UserContext';
+// import { useFollowersCount } from '../hooks/useFollowersCount';
+// import { useFollowingCount } from '../hooks/useFollowingCount';
+// import { useHodlingCount } from '../hooks/useHodlingCount';
+// import { useListedCount } from '../hooks/useListedCount';
+// import { useNewTokens } from '../hooks/useNewTokens';
 
-const PrivateHomePage = dynamic(
-  () => import('../components/layout/PrivateHomePage'),
-  {
-    loading: () => <div>...</div>
-  }
-);
 
-export async function getServerSideProps({ req, res }) {
-  await authenticate(req, res);
+// const PublicHomePage = dynamic(
+//   () => import('../components/layout/PublicHomePage'),
+//   {
+//     loading: () => <div>...</div>
+//   }
+// );
 
-  const limit = 10;
+// const PrivateHomePage = dynamic(
+//   () => import('../components/layout/PrivateHomePage'),
+//   {
+//     loading: () => <div>...</div>
+//   }
+// );
 
-  if (!req.address) {
-    return {
-      props: {
-        address: null,
-        user: null,
-        limit,
-        prefetchedFeed: null
-      }
-    }
-  }
+// export async function getServerSideProps({ req, res }) {
+//   await authenticate(req, res);
 
-  const userPromise = getUser(req.address, req.address);
-  const feed = getActions(req.address, ActionSet.Feed, 0, limit); 
+//   const limit = 10;
 
-  const [
-    user,
-    pfeed,
-  ] = await Promise.all([
-    userPromise,
-    feed,
-  ]);
+//   if (!req.address) {
+//     return {
+//       props: {
+//         address: null,
+//         user: null,
+//         limit,
+//         prefetchedFeed: null
+//       }
+//     }
+//   }
 
-  return {
-    props: {
-      address: req.address || null,
-      user,
-      limit,
-      prefetchedFeed: [pfeed],
-    }
-  }
-}
+//   const userPromise = getUser(req.address, req.address);
+//   const feed = getActions(req.address, ActionSet.Feed, 0, limit); 
 
-export default function Home({
-  address,
-  user,
-  limit,
-  prefetchedFeed,
-}) {
+//   const [
+//     user,
+//     pfeed,
+//   ] = await Promise.all([
+//     userPromise,
+//     feed,
+//   ]);
 
-  const { rankings: mostLiked } = useRankings(true, limit, null, "token");
-  const { rankings: mostFollowed } = useRankings(true, limit, null);
+//   return {
+//     props: {
+//       address: req.address || null,
+//       user,
+//       limit,
+//       prefetchedFeed: [pfeed],
+//     }
+//   }
+// }
 
-  const { results: newUsers } = useNewUsers(limit, null);
-  const { results: newTokens } = useNewTokens(limit, null);
+// export default function Home({
+//   address,
+//   user,
+//   limit,
+//   prefetchedFeed,
+// }) {
 
-  const { actions: feed } = useActions(user?.address, ActionSet.Feed, limit, prefetchedFeed);
+//   const { rankings: mostLiked } = useRankings(true, limit, null, "token");
+//   const { rankings: mostFollowed } = useRankings(true, limit, null);
 
-  const [hodlingCount] = useHodlingCount(user?.address, null);
-  const [listedCount] = useListedCount(user?.address, null);
+//   const { results: newUsers } = useNewUsers(limit, null);
+//   const { results: newTokens } = useNewTokens(limit, null);
 
-  const [followersCount] = useFollowersCount(user?.address, null);
-  const [followingCount] = useFollowingCount(user?.address, null);
+//   const { actions: feed } = useActions(user?.address, ActionSet.Feed, limit, prefetchedFeed);
 
+//   const [hodlingCount] = useHodlingCount(user?.address, null);
+//   const [listedCount] = useListedCount(user?.address, null);
+
+//   const [followersCount] = useFollowersCount(user?.address, null);
+//   const [followingCount] = useFollowingCount(user?.address, null);
+
+//   return (
+//     <>
+//       <Head>
+//         <title>Hodl My Moon</title>
+//       </Head>
+//       <RankingsContext.Provider value={{
+//         limit,
+//         mostFollowed,
+//         mostLiked,
+//         newUsers,
+//         newTokens
+//       }}>
+//         {!address &&
+//           <Container maxWidth="xl">
+//             <PublicHomePage />
+//           </Container>
+//         }
+//         {address &&
+//           <FeedContext.Provider
+//             value={{ feed }}>
+//             <UserContext.Provider
+//               value={{
+//                 hodlingCount,
+//                 listedCount,
+//                 followersCount,
+//                 followingCount
+//               }}
+//             >
+//               <Container maxWidth="xl">
+//                 <PrivateHomePage user={user} address={address} />
+//               </Container>
+//             </UserContext.Provider>
+//           </FeedContext.Provider>
+//         }
+//       </RankingsContext.Provider>
+//     </>
+//   )
+// }
+
+export default function Home({ }) {
   return (
-    <>
-      <Head>
-        <title>Hodl My Moon</title>
-      </Head>
-      <RankingsContext.Provider value={{
-        limit,
-        mostFollowed,
-        mostLiked,
-        newUsers,
-        newTokens
-      }}>
-        {!address &&
-          <Container maxWidth="xl">
-            <PublicHomePage />
-          </Container>
-        }
-        {address &&
-          <FeedContext.Provider
-            value={{ feed }}>
-            <UserContext.Provider
-              value={{
-                hodlingCount,
-                listedCount,
-                followersCount,
-                followingCount
-              }}
-            >
-              <Container maxWidth="xl">
-                <PrivateHomePage user={user} address={address} />
-              </Container>
-            </UserContext.Provider>
-          </FeedContext.Provider>
-        }
-      </RankingsContext.Provider>
-    </>
+    <h1>test</h1>
   )
-}
+} 
