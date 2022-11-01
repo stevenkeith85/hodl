8fe282c108d3f28db21b234fa2cab1d28927759e
Steven Keith <ext_steven.keith@coats.com>
2022-11-01 09:30:07 +0000

    removed mailchimp form

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,7 +4,7 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import RedditIcon from "@mui/icons-material/Reddit";
 import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
 import TwitterIcon from "@mui/icons-material/Twitter";
-     
+
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import { grey } from "@mui/material/colors";
@@ -26,231 +26,157 @@ export default function LoginPage({ hasReadPermission }) {
                 <title>{hasReadPermission ? 'Logout' : 'Login'}</title>
                 <meta name="robots" content="noindex" />
             </Head>
-            <Box sx={{
+            <div style={{
                 display: 'flex',
+                minHeight: '100vh',
+                width: '100vw',
                 flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center',
-                height: { xs: `calc(100vh - 500px)`, sm: `calc(100vh - 250px)` },
-                minHeight: 'max(70vh, 450px)',
             }}>
-                <Box
-                    sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                    }}>
+                <Box sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    flex: 1,
+                }}>
                     <Box
-                        component="span"
                         sx={{
-                            fontFamily: theme => theme.logo.fontFamily,
+                            display: 'flex',
+                            alignItems: 'center',
+                        }}>
+                        <Box
+                            component="span"
+                            sx={{
+                                fontFamily: theme => theme.logo.fontFamily,
+                                fontSize: {
+                                    xs: 35,
+                                    sm: 40,
+                                    md: 45
+                                },
+                                fontWeight: 600,
+                                color: theme => theme.palette.primary.main,
+                            }}>
+                            Hodl My Moon
+
+                        </Box>
+                        <RocketLaunchIcon sx={{
+                            marginLeft: 2,
+                            color: grey[500],
                             fontSize: {
                                 xs: 35,
                                 sm: 40,
                                 md: 45
                             },
-                            fontWeight: 600,
-                            color: theme => theme.palette.primary.main,
-                        }}>
-                        Hodl My Moon
-
+                        }} />
                     </Box>
-                    <RocketLaunchIcon sx={{
-                        marginLeft: 2,
-                        color: grey[500],
-                        fontSize: {
-                            xs: 35,
-                            sm: 40,
-                            md: 45
-                        },
-                    }} />
-                </Box>
-                <Typography
-                    sx={{
-                        marginTop: 1,
-                        marginBottom: 0,
-                        fontFamily: theme => theme.logo.fontFamily,
-                        fontSize: {
-                            xs: 18,
-                            sm: 20,
-                        },
-                        color: '#999',
-                        textAlign: 'center'
-                    }}>
-                    A web3 social network and marketplace
-                </Typography>
-                <Box sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}>
-                    {hasReadPermission ?
-                        <>
-                            <Button
-                                sx={{
-                                    paddingY: 1,
-                                    paddingX: 3,
-                                    fontSize: 16,
-                                    margin: 6
-                                }}
-                                color="secondary"
-                                variant="contained"
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    document.cookie = `${process.env.NEXT_PUBLIC_HODL_MY_MOON_PASSWORD_COOKIE_NAME}=; max-age=; path=`;
-                                    window.location.href = "/login";
-                                }}
-                            >
-                                Logout
-                            </Button>
-                        </>
-                        :
-                        <Box
-                            sx={{
-                                padding: 0,
-                                textAlign: 'center'
-                            }}>
-                            <Box
-                                sx={{
-                                    padding: 4
-                                }}
-                            >
-                                <form>
-                                    <Box
-                                        sx={{ display: 'flex', flexDirection: 'column' }}
-                                    >
-                                        <TextField
-                                            sx={{
-                                                background: 'white',
-                                                width: {
-                                                    xs: 300,
-                                                },
-                                                marginBottom: 2,
-                                            }}
-                                            placeholder="early access password"
-                                            type="password"
-                                            value={password}
-                                            onChange={(e) => setPassword(e.target.value)}
-                                        />
-                                        <div>
-                                            <Button
-                                                sx={{
-                                                    marginX: 2,
-                                                    paddingY: 1,
-                                                    paddingX: 3,
-                                                    fontSize: 16
-                                                }}
-                                                variant="contained"
-                                                color="secondary"
-                                                type="submit"
-                                                onClick={(e) => {
-                                                    e.preventDefault();
-                                                    document.cookie = `${process.env.NEXT_PUBLIC_HODL_MY_MOON_PASSWORD_COOKIE_NAME}=${password}; max-age=${60 * 60 * 24 * 7}; path=/`;
-                                                    window.location.href = "/";
-                                                }}
-                                            >
-                                                Login
-                                            </Button>
-                                        </div>
-                                    </Box>
-                                </form>
-                            </Box>
-                        </Box>
-                    }
-                </Box>
-            </Box>
-            {!hasReadPermission && <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: {
-                        xs: 'column',
-                        sm: 'row'
-                    }
-                }}>
-                <Box sx={{
-                    width: { xs: '100%%', sm: '50%' },
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    height: '250px',
-                    background: '#fafafa',
-                    boxSizing: 'border-box'
-                }}>
                     <Typography
-                        mb={2}
                         sx={{
-                            fontSize: 20,
-                            color: theme => theme.palette.text.secondary
-                        }}
-                    >I&apos;d like early access...</Typography>
-                    <div>
-                        <form
-                            action="https://hodlmymoon.us9.list-manage.com/subscribe/post?u=ec30c975c5c1d85f780a863c0&amp;id=8528416789&amp;f_id=00f60fe1f0"
-                            method="post"
-                            target="_self">
+                            marginTop: 1,
+                            marginBottom: 0,
+                            fontFamily: theme => theme.logo.fontFamily,
+                            fontSize: {
+                                xs: 18,
+                                sm: 20,
+                            },
+                            color: '#999',
+                            textAlign: 'center'
+                        }}>
+                        A web3 social network and marketplace
+                    </Typography>
+                    <Box sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}>
+                        {hasReadPermission ?
+                            <>
+                                <Button
+                                    sx={{
+                                        paddingY: 1,
+                                        paddingX: 3,
+                                        fontSize: 16,
+                                        margin: 6
+                                    }}
+                                    color="secondary"
+                                    variant="contained"
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        document.cookie = `${process.env.NEXT_PUBLIC_HODL_MY_MOON_PASSWORD_COOKIE_NAME}=; max-age=; path=`;
+                                        window.location.href = "/login";
+                                    }}
+                                >
+                                    Logout
+                                </Button>
+                            </>
+                            :
                             <Box
                                 sx={{
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    alignItems: 'center'
+                                    padding: 0,
+                                    textAlign: 'center'
                                 }}>
-                                <TextField
+                                <Box
                                     sx={{
-                                        marginBottom: 2,
-                                        background: 'white',
-                                        width: {
-                                            maxWidth: '90%',
-                                            xs: 300,
-                                        }
+                                        padding: 4
                                     }}
-                                    placeholder="person@example.com"
-                                    type="email"
-                                    name="EMAIL"
-                                />
-                                <Box sx={{
-                                    display: 'none'
-                                }}>
-                                    <input
-                                        type="text"
-                                        name="b_ec30c975c5c1d85f780a863c0_8528416789"
-                                        value=""
-                                    />
+                                >
+                                    <form>
+                                        <Box
+                                            sx={{ display: 'flex', flexDirection: 'column' }}
+                                        >
+                                            <TextField
+                                                sx={{
+                                                    background: 'white',
+                                                    width: {
+                                                        xs: 300,
+                                                    },
+                                                    marginBottom: 2,
+                                                }}
+                                                type="password"
+                                                value={password}
+                                                onChange={(e) => setPassword(e.target.value)}
+                                            />
+                                            <div>
+                                                <Button
+                                                    sx={{
+                                                        marginX: 2,
+                                                        paddingY: 1,
+                                                        paddingX: 3,
+                                                        fontSize: 16
+                                                    }}
+                                                    variant="contained"
+                                                    color="secondary"
+                                                    type="submit"
+                                                    onClick={(e) => {
+                                                        e.preventDefault();
+                                                        document.cookie = `${process.env.NEXT_PUBLIC_HODL_MY_MOON_PASSWORD_COOKIE_NAME}=${password}; max-age=${60 * 60 * 24 * 7}; path=/`;
+                                                        window.location.href = "/";
+                                                    }}
+                                                >
+                                                    Login
+                                                </Button>
+                                            </div>
+                                        </Box>
+                                    </form>
                                 </Box>
-                                <div>
-                                    <Button
-                                        sx={{
-                                            marginX: 2,
-                                            paddingY: 1.25,
-                                            paddingX: 3,
-                                            fontSize: 16
-                                        }}
-                                        color="info"
-                                        variant="outlined"
-                                        type="submit"
-                                        name="subscribe"
-                                    >
-                                        Submit
-                                    </Button>
-                                </div>
                             </Box>
-                        </form>
-                    </div>
+                        }
+                    </Box>
                 </Box>
                 <Box sx={{
                     display: 'flex',
-                    width: { xs: '100%', sm: '50%' },
+                    width: `100%`,
                     justifyContent: 'center',
                     alignItems: 'center',
-                    height: '250px',
                     background: '#f6f6f6',
-                    boxSizing: 'border-box'
+                    boxSizing: 'border-box',
+                    padding: 2
                 }}>
                     <Link href="https://twitter.com/hodlmymoon">
                         <TwitterIcon
                             sx={{
                                 marginX: { xs: 1, sm: 2 },
-                                fontSize: 30,
+                                fontSize: 22,
                                 color: grey[500]
                             }} />
                     </Link>
@@ -258,8 +184,8 @@ export default function LoginPage({ hasReadPermission }) {
                         <Box
                             sx={{
                                 marginX: { xs: 1, sm: 2 },
-                                width: 30,
-                                height: 30
+                                width: 22,
+                                height: 22
                             }}>
                             <TikTokIcon
                                 color={grey[500]}
@@ -270,7 +196,7 @@ export default function LoginPage({ hasReadPermission }) {
                         <RedditIcon
                             sx={{
                                 marginX: { xs: 1, sm: 2 },
-                                fontSize: 30,
+                                fontSize: 22,
                                 color: grey[500]
                             }} />
                     </Link>
@@ -278,7 +204,7 @@ export default function LoginPage({ hasReadPermission }) {
                         <FacebookIcon
                             sx={{
                                 marginX: { xs: 1, sm: 2 },
-                                fontSize: 30,
+                                fontSize: 22,
                                 color: grey[500]
                             }} />
                     </Link>
@@ -286,12 +212,12 @@ export default function LoginPage({ hasReadPermission }) {
                         <InstagramIcon
                             sx={{
                                 marginX: { xs: 1, sm: 2 },
-                                fontSize: 30,
+                                fontSize: 22,
                                 color: grey[500]
                             }} />
                     </Link>
                 </Box>
-            </Box>}
+            </div>
         </>
     )
 }
