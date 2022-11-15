aad051d9e401d6f68179520ce31d7f281632becf
Steven Keith <stevenkeith@hotmail.com>
2022-11-15 19:58:14 +0000

    bulletproof

--- src/components/avatar/UserHandle.tsx
+++ src/components/avatar/UserHandle.tsx
@@ -11,6 +11,11 @@ interface UserHandle {
 }
 
 export const UserHandle: FC<UserHandle> = ({ user, fontSize }) => {
+
+    if (!user) {
+        return null;
+    }
+    
     return (<>
         <Typography sx={{ fontSize }}>
             {user?.nickname ? truncateText(user.nickname, 10) : getShortAddress(user?.address)}
