dc3ea7c403f79cf5c17e778b55c3ea73e57f9d5e
Steven Keith <stevenkeith@hotmail.com>
2022-11-24 12:12:53 +0000

    testing vercel url here

--- src/pages/api/sitemap.ts
+++ src/pages/api/sitemap.ts
@@ -3,7 +3,7 @@ import { zRange } from "../../lib/database/rest/zRange";
 
 
 export default async function route (req: NextRequest) {
-    const domain = process.env.VERCEL_URL || "https://www.hodlmymoon.com";
+    const domain = `https://${process.env.VERCEL_URL || 'www.hodlmymoon.com'}`;
 
     const pages = ['/', '/explore', '/about', '/contact'];
 
