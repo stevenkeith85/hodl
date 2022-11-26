0c4a8c50a6efb05bc59505e6685a8708a95a200a
Steven Keith <stevenkeith@hotmail.com>
2022-11-26 19:28:41 +0000

    should hopefully fix mobile

--- src/lib/connections.ts
+++ src/lib/connections.ts
@@ -1,32 +1,19 @@
+import detectEthereumProvider from '@metamask/detect-provider'
+
 export const getSigner = async () => {
   try {
+    const { Web3Provider } = await import('@ethersproject/providers');
 
-  
-  const { Web3Provider } = await import('@ethersproject/providers');
+    const metamaskEthereumProvider = await detectEthereumProvider();
 
-  // @ts-ignore
-  if (window?.ethereum) {
-    alert("window.ethereum is available")
-  } else {
-    alert("window.ethereum is not available")
+    const provider = new Web3Provider(metamaskEthereumProvider);
+    
+    await provider.send("eth_requestAccounts", []);
+
+    const signer = provider.getSigner()
+
+    return signer;
+  } catch (e) {
+    alert(e.message)
   }
-  
-  // A Web3Provider wraps a standard Web3 provider, which is
-  // what MetaMask injects as window.ethereum into each page
-  // @ts-ignore
-  const provider = new Web3Provider(window.ethereum)
-
-  // MetaMask requires requesting permission to connect users accounts
-  const addresses = await provider.send("eth_requestAccounts", []);
-
-  alert(JSON.stringify(addresses))
-  // The MetaMask plugin also allows signing transactions to
-  // send ether and pay to change state within the blockchain.
-  // For this, you need the account signer...
-  const signer = provider.getSigner()
-
-  return signer;
-} catch(e) {
-  alert(e.message)
-}
 }
