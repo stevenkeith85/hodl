bb9608ec30ee0d810c128440b8d1d14f3f4e714f
Steven Keith <stevenkeith@hotmail.com>
2023-02-22 11:29:39 +0000

    this should have been checked in

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -25,13 +25,13 @@ module.exports = {
     //   url: `${process.env.GANACHE_URL}`,
     //   accounts: [process.env.WALLET_PRIVATE_KEY]
     // },
-    mumbai: {
-      url: `${process.env.ALCHEMY_MUMBAI_URL}/${process.env.ALCHEMY_MUMBAI_AUTHENTICATION_TOKEN}/`,
-      accounts: [process.env.WALLET_PRIVATE_KEY]
-    },
-    // mainnet: {
-    //   url: `${process.env.ALCHEMY_MAINNET_URL}/${process.env.ALCHEMY_MAINNET_AUTHENTICATION_TOKEN}/`,
-    //   accounts: [process.env.WALLET_PRIVATE_KEY],
-    // }
+    // mumbai: {
+    //   url: `${process.env.ALCHEMY_MUMBAI_URL}/${process.env.ALCHEMY_MUMBAI_AUTHENTICATION_TOKEN}/`,
+    //   accounts: [process.env.WALLET_PRIVATE_KEY]
+    // },
+    mainnet: {
+      url: `${process.env.ALCHEMY_MAINNET_URL}/${process.env.ALCHEMY_MAINNET_AUTHENTICATION_TOKEN}/`,
+      accounts: [process.env.WALLET_PRIVATE_KEY],
+    }
   },
 };
